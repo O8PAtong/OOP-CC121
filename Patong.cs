71dcee7d1ef874cfb3f5c8990d42bbdb69f557bf
PATONG JESSON B <88761730+O8PAtong@users.noreply.github.com>
2021-08-20 23:51:46 +0800

    First Commit

---
+++ Patong.cs
@@ -0,0 +1,31 @@
+using System;
+
+using System.IO;
+
+using System.Linq;
+
+using System.Collections.Generic;
+
+
+
+namespace CSharp_Shell
+
+{
+
+
+
+    public static class Program
+
+    {
+
+        public static void Main()
+
+        {
+
+            Console.WriteLine("Hello, World!");
+
+        }
+
+    }
+
+}
